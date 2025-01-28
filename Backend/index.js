9ba2178df4444db11b902d9781235afffc2630b0
HanzalaArshad <hanzalaarshad338@gmail.com>
2025-01-28 15:34:47 +0500

    updated cors

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin:['http://localhost:5173','https://booknest-hanzala.vercel.app/'],
+  origin:['http://localhost:5173','https://booknest-hanzala.vercel.app'],
   credentials: true
 
 }))
